588cbb6e0148e04c5525eecfed74bf672b6d696a
Lucio Torre <lucio.torre@gmail.com>
2020-05-10 20:57:59 +0100

    Update pedestrians.py

--- samples/pedestrians.py
+++ samples/pedestrians.py
@@ -1,15 +1,20 @@
 from synthingie import Module, Player
 
 
-SAMPLERATE = 48000
-FRAMESIZE = 1024
+def play():
+    SAMPLERATE = 48000
+    FRAMESIZE = 1024
 
-mod = Module(SAMPLERATE, FRAMESIZE)
+    mod = Module(SAMPLERATE, FRAMESIZE)
 
-osc = mod.sin(2500, 0.2)
-gate = mod.naive_square(5, amplitude=0.5) + 0.5
+    osc = mod.sin(2500, 0.2)
+    gate = mod.naive_square(5, amplitude=0.5) + 0.5
 
-pedestrian = osc * gate
+    pedestrian = osc * gate
 
-with Player(mod) as p:
-    p.play(pedestrian)
+    with Player(mod) as p:
+        p.play(pedestrian)
+
+
+if __name__ == "__main__":
+    play()
